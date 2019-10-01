018b64a9f922f3318d2eaa3a95ec11a0e11e21ea
Cody Tividad <theeclipse162@gmail.com>
2019-10-01 17:20:54 -0600

    Added Hexagon object

---
+++ Hexagon.java
@@ -0,0 +1,19 @@
+public class Hexagon extends Shape{
+	
+	public Hexagon(){
+		super();
+	}
+
+	public Hexagon(int x, int y){
+		super(x,y);
+	}
+
+	public int getX(){ return this.x;}
+
+	public void setX(int x){ this.x = x;}
+
+	public int getY(){ return this.y;}
+
+	public void setY(int y){ this.y = y;}
+
+
