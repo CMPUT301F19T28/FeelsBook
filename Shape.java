6d86eeb886b2946d50c895d978a3967338ede0b4
Daylen Pacheco <44179855+d-pacheco@users.noreply.github.com>
2019-10-01 17:58:53 -0600

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,14 +1,17 @@
 public class Shape{
     private int x;
     private int y;
+    private String color;
 
     public Shape(){
         this.x = 0;
         this.y = 0;
+        this.color = "red";
     }
   
-    public Shape(int x, int y){
+    public Shape(int x, int y, String color){
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
