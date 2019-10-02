baf6d2f8eaadcbe2e380e70d6b265b2cb353c8b6
Cody Tividad <theeclipse162@gmail.com>
2019-10-01 18:35:11 -0600

    Cleaned up Shape.java

--- Shape.java
+++ Shape.java
@@ -6,10 +6,7 @@ public class Shape{
     public Shape(){
         this.x = 0;
         this.y = 0;
-<<<<<<< HEAD
-=======
-        this.color = "$This_is_a_color";
->>>>>>> 4a5e2674cf81cd89a3f379d92a475cf288bb29d7
+        this.color = "This_is_a_color";
     }
   
     public Shape(int x, int y, String color){
