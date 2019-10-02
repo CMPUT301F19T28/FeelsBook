0ff0fd2c5b59f65a274ca52834a41678d42afbb4
Cody Tividad <theeclipse162@gmail.com>
2019-10-01 18:18:58 -0600

    Added updated Shape class

--- Shape.java
+++ Shape.java
@@ -6,12 +6,11 @@ public class Shape{
     public Shape(){
         this.x = 0;
         this.y = 0;
-        this.color = "red";
     }
   
     public Shape(int x, int y, String color){
         this.x = x;
         this.y = y;
-        this.color = color;
+	this.color = "red";
     }
 }
