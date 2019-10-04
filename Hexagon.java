9ffb5fba2e8f6ecbd3470b8fdfefcb281dccd889
kbalisnomo <kylephil@ualberta.ca>
2019-10-04 11:15:35 -0600

    Changed classes

--- Hexagon.java
+++ Hexagon.java
@@ -1,20 +1,4 @@
 public class Hexagon extends Shape{
-	private Shape color = "blue"
-	
-	public Hexagon(){
-		super();
-	}
-
-	public Hexagon(int x, int y){
-		super(x,y);
-	}
-
-	public int getX(){ return this.x;}
-
-	public void setX(int x){ this.x = x;}
-
-	public int getY(){ return this.y;}
-
-	public void setY(int y){ this.y = y;}
+}
 
 
--- Shape.java
+++ Shape.java
@@ -1,14 +1,4 @@
 public class Shape{
     private int x;
     private int y;
-
-    public Shape(){
-        this.x = 0;
-        this.y = 0;
-    }
-  
-    public Shape(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
 }
