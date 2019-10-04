4b59162b4ebb37dca8d11ba605514473b7197f10
JoeBosch <boschjoseph1000@gmail.com>
2019-10-03 21:26:32 -0600

    Removed Constructors from Shape

--- app/src/main/java/com/cmput/feelsbook/Shape.java
+++ app/src/main/java/com/cmput/feelsbook/Shape.java
@@ -3,17 +3,4 @@ package com.cmput.feelsbook;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color;
-
-    public Shape(){
-        this.x = 0;
-        this.y = 0;
-        this.color = "This_is_a_color";
-    }
-
-    public Shape(int x, int y, String color){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-    }
 }
